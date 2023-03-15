78a0c841029df894376b76a4fb977456c8f8d1fa
Ihor Tkachenko <Igor.tt6@gmail.com>
2023-03-15 14:11:52 +0200

    bug fixed

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import VideoPlayer from "./modules/playVideo";
 import MiniSlider from "./modules/sliders/slider-mini";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
+import ShowInfo from "./modules/sliders/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({ btns: '.next', container: '.page', animationDuration: '200' });
@@ -44,4 +46,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 })
---
+++ src/js/modules/download.js
@@ -0,0 +1,31 @@
+export default class Download {
+    constructor(triggers) {
+        this.btns = document.querySelectorAll(triggers);
+        this.path = 'assets/img/mainbg.jpg';
+        this.downloadItem = this.downloadItem.bind(this)
+    }
+
+    downloadItem(path) {
+        const element = document.createElement('a');
+
+        element.setAttribute('href', path);
+        element.setAttribute('download', 'nice_picture');
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    init() {
+        this.btns.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation()
+                this.downloadItem(this.path);
+            })
+        });
+    }
+
+
+};
